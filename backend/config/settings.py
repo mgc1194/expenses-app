56b020d219bdb779ec0ab905bdbe3de3a1f23f8d
Mario Gil Correa <mario.gil-correa@code.org>
2026-05-15 20:54:46 -0400

    Setting up Railway for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,6 +158,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR.parent / 'frontend' / 'dist']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR.parent / 'frontend' / 'dist']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,13 +18,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from config.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
+    re_path(r'^(?!api/|admin/|static/).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
